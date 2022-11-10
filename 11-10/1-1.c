894b85750758ec2407bb4271adac87d109609734
FeiFei0827 <114592928+FeiFei0827@users.noreply.github.com>
2022-11-10 20:31:41 +0800

    Add files via upload

---
+++ 11-10/1-1.c
@@ -0,0 +1,64 @@
+//1-1
+//int main()
+//{
+//	printf("15减去37的结果是%d\n", 15 - 37);
+////
+//}
+//1-2
+//int main()
+//{
+//	printf("天\n地\n人\n");
+//}
+//1-3
+//int main()
+//{
+//
+//	printf("喂！\n\n您好！\n再见。\n");
+//}
+//1-4
+//int main()
+//{
+//	int a = 3.14;
+//	printf("%d", a);
+//}
+//1-5
+//int main()
+//{
+//	int a;
+//	printf("输入一个整数\n");
+//	scanf("%d", &a);
+//	printf("%d\n", a = 12);
+//}
+//1-6
+//int main()
+//{
+//	int a;
+//	printf("输入一个整数\n");
+//	scanf("%d", &a);
+//	printf("%d\n", a - 6);
+//}
+//1-7
+//int main()
+//{
+//	puts("天");
+//	puts("地");
+//	puts("人");
+//
+//}
+//1-8
+//int main()
+//{
+//	int a, b;
+//	printf("输入整数"); scanf("%d", &a);
+//	printf("输入整数"); scanf("%d", &b);
+//	printf("他们的乘积是%d\n", a*b);
+//}
+//1-9
+//int main()
+//{
+//	int a, b, c;
+//	printf("输入整数"); scanf("%d", &a);
+//	printf("输入整数"); scanf("%d", &b);
+//	printf("输入整数"); scanf("%d", &c);
+//	printf("它们的和是%d\n", a+b+c);
+//}
---
+++ 11-10/2.c
@@ -0,0 +1,49 @@
+//2-1
+//int main()
+//{
+//	int x, y;
+//	printf("请输入两个整数\n");
+//	printf("输入整数x"); scanf("%d", &x);
+//	printf("输入整数y"); scanf("%d", &y);
+//	printf("x的值是y的%%%d", x*100 / y );
+//	
+//}
+//2-2
+//int main()
+//{
+//	int x, y;
+//	printf("请输入两个整数\n");
+//	printf("输入整数x"); scanf("%d", &x);
+//	printf("输入整数y"); scanf("%d", &y);
+//	printf("他们的和是%d,积是%d", x + y, x*y);
+//}
+//2-3
+//int main()
+//{
+//	double a;
+//	printf("请输入一个实数：");   scanf("%lf", &a);
+//	printf("你输入的是%f", a);//                                                  不能用float要用double 不然会溢出
+//}
+//2-4
+//int main()
+//{
+//
+//
+//}
+//2-5
+//int main()
+//{
+//	double a, b;
+//	printf("输入两个整数\n");
+//	printf("整数a:"); scanf("%lf", &a);
+//	printf("整数b:"); scanf("%lf", &b);
+//	printf("a是b的%%%f", a / b * 100);
+//
+//2-6
+//int main()
+//{
+//	double a;
+//	printf("输入您的身高"); scanf("%lf", &a); // 使用%lf时 要记得定义时用double
+//	printf("您的标准体重是%5.1f", (a - 100)*0.9);      %m.n 显示至少m位浮点数，但小数点后只显示一位。
+//}
+//
