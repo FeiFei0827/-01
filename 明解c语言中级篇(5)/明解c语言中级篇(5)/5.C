ed09acdebecef4d2adf22c67c85d1da6e8e80aea
FeiFei0827 <114592928+FeiFei0827@users.noreply.github.com>
2022-12-01 19:48:56 +0800

    Add files via upload

---
+++ 明解c语言中级篇(5)/明解c语言中级篇(5)/5.C
@@ -0,0 +1,71 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <time.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//#define MAX_STAGE	10				/* 关卡数 */
+//#define LEVEL_MIN	 3				/* 最低等级（字母个数）*/
+//#define LEVEL_MAX	20				/* 最高等级（字母个数）*/
+//
+///*--- 等待x毫秒 ---*/
+//int sleep(unsigned long x)
+//{
+//	clock_t c1 = clock(), c2;
+//
+//	do {
+//		if ((c2 = clock()) == (clock_t)-1)	/* 错误 */
+//			return 0;
+//	} while (1000.0 * (c2 - c1) / CLOCKS_PER_SEC < x);
+//	return 1;
+//}
+//int main(void)
+//{
+//	int i, stage;
+//	int level;						/* 等级（数值的位数）*/
+//	int success = 0;				/* 答对数量 */
+//	clock_t start, end;					/* 开始时间/结束时间 */
+//	const char ltr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"		/* 大写英文字母 */
+//		"abcdefghijklmnopqrstuvwxyz";	/* 小写英文字母 */
+//
+//	srand(time(NULL));					/* 设定随机数的种子 */
+//
+//	printf("英文字母记忆训练\n");
+//
+//	do {
+//		printf("要挑战的等级(%d～%d)：", LEVEL_MIN, LEVEL_MAX);
+//		scanf("%d", &level);
+//	} while (level < LEVEL_MIN || level > LEVEL_MAX);
+//
+//	printf("来记忆%d个英文字母吧。\n", level);
+//
+//	start = clock();
+//	for (stage = 0; stage < MAX_STAGE; stage++) {
+//		char mstr[LEVEL_MAX + 1];			/* 需要记忆的一串英文字母 */
+//		int x;				/* 读取到的一串英文字母 */
+//
+//		for (i = 0; i < level; i++)			/* 生成作为题目的字符串 */
+//			mstr[i] = ltr[rand() % strlen(ltr)];
+//		mstr[level] = '\0';
+//
+//		printf("%s", mstr);
+//		fflush(stdout);
+//		sleep(125 * level);					/* 问题提示125 × level毫秒 */
+//		i = rand() % level;
+//		printf("\r%*s\r请输入%c字母是第几号", level, "",mstr[i]);
+//		fflush(stdout);
+//		scanf("%d", &x);
+//		if (x != i+1)
+//			printf("\a回答错误。\n");
+//		else {
+//			printf("回答正确。\n");
+//			success++;
+//		}
+//	}
+//	end = clock();
+//
+//	printf("%d次中答对了%d次。\n", MAX_STAGE, success);
+//	printf("用时%.1f秒。\n", (double)(end - start) / CLOCKS_PER_SEC);
+//
+//	return 0;
+//}
